5e3ae20473cc77120bdc1e3ea3a0fee5b4f0b163
Chalapathi-Neo <avula.chalapati@neosoftmail.com>
2020-11-23 17:53:20 +0530

    first ssr commit

---
+++ src/client/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = ( ) => {
+  return (
+    <div>
+      <div className="container">
+        Hiiiiiiiiiiiiiiii
+      </div>
+    </div>
+  );
+};
+
+export default {
+  component: App
+};
